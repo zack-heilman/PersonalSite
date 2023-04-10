34f07ca8beed0c0d34d616aed34c7f5e80cbea66
Zackary Heilman <zrobertheilman@gmail.com>
2023-04-10 01:30:59 -0700

    Force npm update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-    assetPrefix: isProd ? '/PersonalSite/' : '',
     reactStrictMode: true,
     images: {
         deviceSizes: [640, 768, 1024, 1280, 1536],
