1ed1dedad57f656aee1f81d98ed3ba635d742da6
Zackary Heilman <zrobertheilman@gmail.com>
2023-05-29 21:40:56 -0700

    Updates to resume and removing large picture.

--- src/components/containers/AboutSection.jsx
+++ src/components/containers/AboutSection.jsx
@@ -12,37 +12,7 @@ const AboutSection = () => {
   if (!data) return null;
 
   return (
-    <div className="grid grid-cols-2 items-center gap-7">
-      <div className="col-span-2 lg:col-span-1">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 0.4, delay: 0.2 }}
-          variants={childrenAnimation}
-          className="about-image overflow-hidden rounded-lg"
-        >
-          <div className="about-image-inner fiximage relative border-10 border-primary border-opacity-20">
-            <span className="absolute -top-2.5 left-0 z-10 h-2.5 w-10 animate-ledgerleftright rounded-full bg-gradient-to-r from-transparent to-primary"></span>
-            <span className="absolute top-auto -bottom-2.5 left-auto z-10 h-2.5 w-10 animate-ledgerrightleft rounded-full bg-gradient-to-r from-primary to-transparent"></span>
-            <span className="absolute -left-2.5 top-auto z-10 h-10 w-2.5 animate-ledgerbottomtop rounded-full bg-gradient-to-t from-transparent to-primary"></span>
-            <span className="absolute left-auto -right-2.5 z-10 h-10 w-2.5 animate-ledgertopbottom rounded-full bg-gradient-to-b from-transparent to-primary"></span>
-            <Image
-              loader={imageLoader}
-              unoptimized={true}
-              src={data.largeImage}
-              height={222}
-              width={222}
-              layout="responsive"
-              alt={data.fullName}
-              placeholder="blur"
-              blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                shimmer(660, 422)
-              )}`}
-            />
-          </div>
-        </motion.div>
-      </div>
+      
       <div className="col-span-2 lg:col-span-1">
         <motion.div
           initial="hidden"
@@ -83,7 +53,6 @@ const AboutSection = () => {
           </a>
         </motion.div>
       </div>
-    </div>
   );
 };
 
