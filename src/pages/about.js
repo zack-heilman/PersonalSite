150db6c2594798bf5130e0815a72ba621aac0f01
Zack Heilman <zrobertheilman@gmail.com>
2023-11-21 17:08:16 -0800

    Cleanup of unused components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import {
   AboutSection,
-  ReviewsSection,
-  ServicesSection,
 } from "../components/containers";
 import { Layout2 } from "../components/layout";
 import { SectionHeading } from "../components/utils";
@@ -22,38 +20,6 @@ const about = () => {
         </div>
       </section>
       {/* End About Section */}
-
-      {/* Start Service Section */}
-      <section
-        name="section-service"
-        className="services-section pt-24 lg:pt-28 xl:pt-32"
-      >
-        <div className="container mx-auto">
-          <SectionHeading
-            title="My Services"
-            watermark="Services"
-            animated={false}
-          />
-          <ServicesSection />
-        </div>
-      </section>
-      {/* End Service Section */}
-
-      {/* Start Reviews Section */}
-      <section
-        name="section-reviews"
-        className="reviews-section pt-24 lg:pt-28 xl:pt-32"
-      >
-        <div className="container mx-auto">
-          <SectionHeading
-            title="Client Reviews"
-            watermark="Reviews"
-            animated={false}
-          />
-          <ReviewsSection />
-        </div>
-      </section>
-      {/* End Reviews Section */}
 
       <span className="block pb-24 lg:pb-28 xl:pb-32"></span>
     </Layout2>
