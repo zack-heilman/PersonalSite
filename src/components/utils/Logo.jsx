3a25e9063168bba50761d040f57a1738c647d7c1
Zack Heilman <zrobertheilman@gmail.com>
2023-11-21 21:50:30 -0800

    Cleanup of unused components

--- src/components/utils/Logo.jsx
+++ src/components/utils/Logo.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const Logo = ({ url = "/", text = true }) => {
   return (
      
-      <span className="text-4xl font-bold uppercase leading-none text-primary">
+      <div>
         Z.H.
-      </span>
+      </div>
         
   );
 };
--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,6 +1,5 @@
 import Logo from "./Logo";
 import SectionHeading from "./SectionHeading";
 import Spinner from "./Spinner";
-import Comments from "./Comments";
 
-export { Logo, SectionHeading, Spinner, Comments };
+export { Logo, SectionHeading, Spinner };
